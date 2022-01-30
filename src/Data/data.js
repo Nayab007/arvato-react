a1c63deb004ec5d74830701b46c8a7cef4137fdb
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-01-30 14:07:07 +0100

    new changes

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,114 +1,62 @@
 import React from 'react';
-
 import './Data.css';
 
-
-
-
 /* Here I have all the data Info for the cards, This data  can be export to other components as well */
-const Data = ({Data}) => {
+const Data = ({ Data }) => {
   return (
-  
     <div data-testid="data" className="container">
       <div className="card">
         <div className="content">
           <h2>£35</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum
-          </p>
-          
-          <a href="/moreinfo">More Info</a>
-         
+          <h3>Monthly Plan</h3>
         </div>
       </div>
       <div className="card">
         <div className="content">
           <h2>£45</h2>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum
-          </p>
-          <a href="/moreinfo">More Info</a>
+          <h3>Monthly Plan</h3>
         </div>
       </div>
       <div className="card">
         <div className="content">
           <h2>£55</h2>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum
-          </p>
-          <a href="/moreinfo">More Info</a>
+          <h3>Monthly Plan</h3>
         </div>
       </div>
       <div className="card">
         <div className="content">
           <h2>£65</h2>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum
-          </p>
-          <a href="/moreinfo">More Info</a>
+          <h3>Monthly Plan</h3>
         </div>
       </div>
       <div className="card">
         <div className="content">
           <h2>£75</h2>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum
-          </p>
-          <a href="/moreinfo">More Info</a>
+          <h3>Monthly Plan</h3>
         </div>
       </div>
       <div className="card">
         <div className="content">
           <h2>£85</h2>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum
-          </p>
-          <a href="/moreinfo">More Info</a>
+          <h3>Monthly Plan</h3>
         </div>
       </div>
       <div className="card">
         <div className="content">
           <h2>£95</h2>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum
-          </p>
-          <a href="/moreinfo">More Info</a>
+          <h3>Monthly Plan</h3>
         </div>
       </div>
       <div className="card">
         <div className="content">
           <h2>£105</h2>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum
-          </p>
-          <a href="/moreinfo">More Info</a>
+          <h3>Monthly Plan</h3>
         </div>
       </div>
     </div>
-   
   );
 };
 
